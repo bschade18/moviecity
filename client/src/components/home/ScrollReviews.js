693eca88f4b459c3c128e81d5b95b9b6e4ede010
Bobby Schade <bschade.18@gmail.com>
2020-07-28 08:40:20 -0500

    added image for empty reviews timeline

--- client/src/components/home/ScrollReviews.js
+++ client/src/components/home/ScrollReviews.js
@@ -1,29 +1,50 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import MobileNav from '../layout/MobileNav';
 import Reviews from './Reviews';
 
-const ScrollReviews = ({ reviews, user, logout }) => (
-  <div className="scroll-container">
-    <div className="main">
-      <div className="scroll-nav">
-        <div className="scroll-heading">
-          <p className="home-title">MovieCity</p>
+const ScrollReviews = ({ reviews, user, logout }) => {
+  const displayReviews = () => {
+    let filterReviews = reviews.filter(
+      (review) =>
+        user.friends.includes(review.username) ||
+        user.username === review.username
+    );
+    if (!filterReviews.length) {
+      return (
+        <Fragment>
+          <img
+            src="https://www.pajiba.com/assets_c/2020/04/Happy-Gilmore-thumb-700x481-224295.png"
+            alt="happy gilmore"
+            className="m-auto empty-tl-image"
+          />
+          <p className="text-center mt-2">
+            Happy is upset you haven't reviewed any movies yet
+          </p>
+          <p className="text-center">
+            Reviews from you and your friends will display here when you do
+          </p>
+        </Fragment>
+      );
+    } else {
+      return filterReviews.map((review) => (
+        <Reviews review={review} key={review._id} />
+      ));
+    }
+  };
+
+  return (
+    <div className="scroll-container">
+      <div className="main">
+        <div className="scroll-nav">
+          <div className="scroll-heading">
+            <p className="home-title">MovieCity</p>
+          </div>
         </div>
+        <div className="movie-scroll">{displayReviews()}</div>
       </div>
-      <div className="movie-scroll">
-        {reviews
-          .filter(
-            (review) =>
-              user.friends.includes(review.username) ||
-              user.username === review.username
-          )
-          .map((review) => (
-            <Reviews review={review} key={review._id} />
-          ))}
-      </div>
+      <MobileNav user={user} logout={logout} />
     </div>
-    <MobileNav user={user} logout={logout} />
-  </div>
-);
+  );
+};
 
 export default ScrollReviews;
