d4c25df637617ce956cc044c425dd97bc7bb4634
Bobby Schade <bschade.18@gmail.com>
2020-07-29 15:06:55 -0500

    add API security packages

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const colors = require('colors');
 const path = require('path');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('xss-clean');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 const dotenv = require('dotenv');
@@ -18,6 +24,29 @@ app.use(express.json());
 
 // file upload
 app.use(fileupload());
+
+// sanitize data
+app.use(mongoSanitize());
+
+// set security headers
+app.use(helmet());
+
+// prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+
+// prevent http param pollution
+app.use(hpp());
+
+// enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
