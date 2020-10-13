45d2f597f0eb4315d24ff70c709c36ce5429bf71
Bobby Schade <bschade.18@gmail.com>
2020-10-13 16:53:41 -0500

    updates to NoResults comp

--- client/src/components/elements/NoResults.js
+++ client/src/components/elements/NoResults.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
-const NoResults = ({ image, text1, text2 }) => {
+const NoResults = ({ image, text1, text2, width, marginTop }) => {
   return (
-    <div>
-      <img src={image} alt="no results" className="m-auto no-results-image" />
+    <div className="no-results px-2" style={{ marginTop }}>
       <p className="text-center mt-2">{text1}</p>
       <p className="text-center">{text2}</p>
+      <img
+        src={image}
+        style={{ width }}
+        alt="no results"
+        className="no-results-image"
+      />
     </div>
   );
+};
+
+NoResults.defaultProps = {
+  width: '80%',
+  marginTop: '1.5rem',
 };
 
 export default NoResults;
