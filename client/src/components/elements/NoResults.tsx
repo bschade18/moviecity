682d476a0c6a2cb30ecf4b41776921aad65b9b2a
Bobby Schade <bschade.18@gmail.com>
2021-09-09 10:02:31 -0500

    refactor NoResults

--- client/src/components/elements/NoResults.tsx
+++ client/src/components/elements/NoResults.tsx
@@ -2,22 +2,18 @@ import '../../styles/NoResults.css';
 import React from 'react';
 
 interface NoResultsProps {
-image: string,
-text1: string,
-text2?: string,
-component?: string
+  image: string;
+  text1: string;
+  text2?: string;
+  component?: string;
 }
 
-const NoResults = ({ image, text1, text2, component } : NoResultsProps) => {
-  return (
-    <div
-      className={component === 'Search' ? 'no-results-search' : 'no-results'}
-    >
-      <p className="text-center mt-2">{text1}</p>
-      <p className="text-center">{text2}</p>
-      <img src={image} alt="no results" className="no-results-image" />
-    </div>
-  );
-};
+const NoResults = ({ component, text1, text2, image }: NoResultsProps) => (
+  <div className={component === 'Search' ? 'no-results-search' : 'no-results'}>
+    <p className="text-center mt-2">{text1}</p>
+    <p className="text-center">{text2}</p>
+    <img src={image} alt="no results" className="no-results-image" />
+  </div>
+);
 
 export default NoResults;
