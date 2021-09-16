b5b494eb8f234810aebf5805cebd48e15fdb6aed
Bobby Schade <bschade.18@gmail.com>
2021-09-16 12:43:43 -0500

    add timestamps to messages

--- client/src/components/messages/ChatMessage.js
+++ client/src/components/messages/ChatMessage.js
@@ -1,10 +1,14 @@
 import '../../styles/ChatMessage.css';
+import { formattedDate, todayOrYesterday, ampmDt } from '../../utils/dates';
 import React from 'react';
 
 const ChatMessage = ({ msg, user, idx, arr, currentMessage }) => {
   const msgSentByUser = msg.user === user._id;
   const prevMsgSentBySameUser = idx > 0 && arr[idx - 1].user === msg.user;
+  const prevMsgUser = arr[idx + 1] && arr[idx + 1].user;
+  const lastMsgInGroup = msg.user !== prevMsgUser;
   const msgClassName = msgSentByUser ? 'recipient' : 'sender';
+  const timestampClassName = msgSentByUser ? 'right' : 'left';
 
   const nonUserProfilePhoto = () => {
     const userIsRecipient = currentMessage.recipient._id === user._id;
@@ -18,6 +22,18 @@ const ChatMessage = ({ msg, user, idx, arr, currentMessage }) => {
     }
   };
 
+  const messageDt = (date) => {
+    let datePrefix;
+    if (todayOrYesterday(date)) {
+      datePrefix = todayOrYesterday(date);
+    } else {
+      datePrefix = formattedDate(date);
+    }
+
+    const ampm = ampmDt(date);
+    return `${datePrefix}, ${ampm}`;
+  };
+
   return (
     <div className={`chat-message ${msgClassName}`} key={msg._id}>
       <img
@@ -28,9 +44,18 @@ const ChatMessage = ({ msg, user, idx, arr, currentMessage }) => {
           ((msgSentByUser || prevMsgSentBySameUser) && 'hide-user-photo')
         }
       />
-      <p className={`chat-message-text ${msgClassName}`} key={msg._id}>
-        {msg.text}
-      </p>
+      <div className={`chat-message-with-timestamp ${timestampClassName}`}>
+        <p className={`chat-message-text ${msgClassName}`} key={msg._id}>
+          {msg.text}
+        </p>
+        <p
+          className={
+            'chat-message-timestamp ' + (!lastMsgInGroup && 'hide-timestamp')
+          }
+        >
+          {messageDt(msg.date)}
+        </p>
+      </div>
     </div>
   );
 };
---
+++ client/src/utils/dates.js
@@ -0,0 +1,31 @@
+export const formattedDate = (date) => {
+  const today = new Date(date);
+  let day = today.getDate();
+  let month = today.getMonth() + 1;
+  let year = String(today.getFullYear()).substring(2);
+
+  return `${month}/${day}/${year}`;
+};
+
+export const todayOrYesterday = (date) => {
+  const today = new Date();
+  const otherDt = new Date(date);
+  if (
+    otherDt.getMonth() == today.getMonth() &&
+    otherDt.getFullYear() == today.getFullYear()
+  ) {
+    if (otherDt.getDate() == today.getDate()) {
+      return 'Today';
+    } else if (otherDt.getDate() + 1 == today.getDate()) {
+      return 'Yesterday';
+    }
+  } else {
+    return false;
+  }
+};
+
+export const ampmDt = (date) => {
+  return new Date(date)
+    .toLocaleTimeString()
+    .replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, '$1$3');
+};
