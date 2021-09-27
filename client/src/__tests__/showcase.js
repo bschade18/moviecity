3e101127a8e73e36aad40b6fc5f638be8435d1e0
Bobby Schade <bschade.18@gmail.com>
2021-09-27 13:16:27 -0500

    add UserSearch form test and update ShowCase form submission test

--- client/src/__tests__/showcase.js
+++ client/src/__tests__/showcase.js
@@ -1,32 +1,39 @@
 import '@testing-library/jest-dom';
-
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import userEvent from '@testing-library/user-event';
 import store from '../store';
-
 import Showcase from '../components/landing/Showcase';
 
 test('submitting the form calls onSubmit with name, username, email, pw, and pw2 ', () => {
   let submittedData;
-  const onSubmit = (data) => {
-    submittedData = data;
-  };
+  const onSubmit = (data) => (submittedData = data);
+
   render(
     <Provider store={store}>
-      <Showcase onSubmit={onSubmit} />
+      <Showcase onSubmit={onSubmit} image="showcase image" />
     </Provider>
   );
-  screen.debug();
 
-  screen.getByPlaceholderText(/new password/i);
-  screen.getByPlaceholderText(/confirm password/i);
-  userEvent.click(screen.getByRole('button', { name: /submit/i }));
-  screen.debug();
+  const name = 'coolio schade';
+  const username = 'coolioboolio123';
+  const email = 'cooooooolio123@gmail.com';
+  const password = 'password123';
+  const password2 = 'password123';
+
+  userEvent.type(screen.getByPlaceholderText(/Name/), name);
+  userEvent.type(screen.getByRole('textbox', { name: /username/i }), username);
+  userEvent.type(screen.getByPlaceholderText(/email/i), email);
+  userEvent.type(screen.getByPlaceholderText(/new password/i), password);
+  userEvent.type(screen.getByPlaceholderText(/confirm password/i), password2);
+  userEvent.click(screen.getByRole('button', { name: /sign up/i }));
 
   expect(submittedData).toEqual({
-    username: 'bobbys',
-    password: 'password',
+    name,
+    username,
+    email,
+    password,
+    password2,
   });
 });
---
+++ client/src/__tests__/usersearch.js
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom';
+
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import userEvent from '@testing-library/user-event';
+import store from '../store';
+import UserSearch from '../components/elements/UserSearch';
+
+test('typing in user search form correctly sets the form value', () => {
+  render(
+    <Provider store={store}>
+      <UserSearch />
+    </Provider>
+  );
+
+  const search = 'bobby';
+
+  let input = screen.getByRole('textbox');
+  userEvent.type(screen.getByRole('textbox'), search);
+
+  expect(search).toEqual(input.value);
+});
